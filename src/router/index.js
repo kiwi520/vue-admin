e94bc32d04a5fdf429ef871a46cb9fd5458c2d24
russcos <chandle@chandles-iMac.local>
2021-06-12 17:51:30 +0800

    新建登录分支 新建登录组件 配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+import login from '@/components/login/login'
+
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/'
+      name: 'login',
+      path: '/',
+      component: login
     }
   ]
 })
